e283f19221e81bc9eafb954eb118545562786bfa
Kevin <mallettk@gmail.com>
2017-09-25 16:41:57 -0400

    title

--- template-parts/header/content-page-title.php
+++ template-parts/header/content-page-title.php
@@ -30,7 +30,7 @@ echo $post->ID;
 
 ?>
 
-<div class="page-header" style="background-image: url(<?php the_field('page_title_image_url',$post->ID); ?>);">
+<div class="page-header" style="background-image: url(<?php the_field('page_title_image_url',9); ?>);">
     <div class="container">
         <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         <?php
