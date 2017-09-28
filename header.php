17e549912aa3c09251dcb908f3095565adbdb1c4
Kevin <mallettk@gmail.com>
2017-09-28 18:03:00 -0400

    Removed meta tags description and author.

--- header.php
+++ header.php
@@ -13,8 +13,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
 <!--<link rel="icon" href="../../../../favicon.ico">-->
 
 <?php wp_head();?>
