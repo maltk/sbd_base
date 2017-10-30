1d92673afdcd28d74c5a220beb4cf02f53055e51
Kevin <mallettk@gmail.com>
2017-10-30 13:02:33 -0400

    update bootstrap assets to 4.0.0-beta-2 and style content page title. Also added feature to select individual page title background.

--- template-parts/header/content-page-title.php
+++ template-parts/header/content-page-title.php
@@ -10,28 +10,22 @@
 ?>
 
 
-<div class="page-header">
-    <div class="container">
-        <?php //the_title( '<h1 class="page-title">', '</h1>' ); ?>
-        <?php
-        //if(get_field("sub-heading") ) : ?>
-            <small><?php //the_field("sub-heading");?></small>
-        <?php
-        //endif;
-        ?>
-    </div>
-</div>
-
 <?php 
 
 global $post;
-echo $post->ID;
 
-echo get_field('page_title_image_url',$post->ID);
+//echo $post->ID;
+
+//echo get_field('page_title_image_url',$post->ID);
 
 ?>
 
-<div class="page-header" style="background-image: url('<?php the_field('page_title_image_url',$post->ID); ?>');">
+<div class="page-header" 
+    <?php 
+        if(the_field('page_title_image_url')) {
+            echo "style='background-image: url('".the_field('page_title_image_url',$post->ID)."');";
+        }
+    ?>>
     <div class="container">
         <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         <?php
