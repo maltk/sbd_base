b1032117cc2796739965b11443d8bede797b86ad
Kevin <mallettk@gmail.com>
2017-10-30 13:31:30 -0400

    page title try 6

--- template-parts/header/content-page-title.php
+++ template-parts/header/content-page-title.php
@@ -33,7 +33,7 @@ global $post;
 
 ?>
 
-<div class="page-header" <?php if(get_field('page_title_image_url')) { echo "style=''"; }?>>
+<div class="page-header" <?php if(get_field('page_title_image_url')) { echo "style=background-image: url('".the_field('page_title_image_url',$post->ID)."');"; }?>>
     <div class="container">
         <?php the_title( '<h1 class="page-title">', '</h1>' ); ?>
         <?php
